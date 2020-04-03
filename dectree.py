79818f94528bf6685891260d46bfc5e52c530c16
Felix Soest <felix@soest-net.de>
2020-04-03 13:40:37 +0200

    Charge ra

--- dectree.py
+++ dectree.py
@@ -1,6 +1,9 @@
 from sklearn.neural_network import MLPRegressor
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import numpy as np
+from sklearn.cluster import KMeans
 # %%
 d1 = pd.read_csv('total1.csv')
 d2 = pd.read_csv('total2.csv')
@@ -10,17 +13,33 @@ d2.drop('Unnamed: 0', axis=1, inplace=True)
 d3 = d1.append(d2)
 # %%
 total_y = d3['Y']
-X_train, X_test, Y_train, Y_test = train_test_split(d3.drop('Y', axis=1), total_y, test_size=0.24)
+cluster = KMeans(4)
+cluster.fit(np.array(total_y).reshape(-1,1))
 # %%
 net = MLPRegressor((200, 50, 10), )
 net.fit(X_train, Y_train)
 # %%
 net.score(X_test, Y_test)
 
-pd.DataFrame(d3['Y']).plot()
-len(d3['Y'])
+
 import matplotlib.pyplot as plt
+plt.scatter(range(len(d3['Y'])), d3['Y'], c=cluster.predict(np.array(total_y).reshape(-1,1)))
 plt.scatter(range(len(d3['Y'])), d3['Y'])
-cats = []
-for row in d3:
-    
+
+# %%
+d3['cat'] =  cluster.predict(np.array(total_y).reshape(-1,1))
+X_train, X_test, Y_train, Y_test = train_test_split(d3.drop(columns=['Y']), categories, test_size=0.23)
+# %%
+d3.iloc[4]
+# %%
+svc = SVC()
+svc.fit(X_train, Y_train)
+
+svc.score(X_test, Y_test)
+svc.predict(X_test)
+# %%
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier
+
+
+d3.to_csv('total_with_cat.csv')
